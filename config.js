b75629eb88ac3c65eaea15457f2ccef49bda9813
AmogussTR <140018710+AmogussTR@users.noreply.github.com>
2023-08-16 18:27:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "Discord Botun Tokeni",
-    id: "Discord Botun ID'si",
+    token: "MTE0MTM5MjE5Nzc0MTE4NzEwMw.GcCCyx.D9drb392LqD-GutF9yj_keetvRdUSYZBfexRZ4",
+    id: "1141392197741187103",
     name: "Exp",
     premiumroleid: "Discord Premium Rol ID'si",
-    logkanalid: "Discord Log Kanal ID'si"
+    logkanalid: "1141388797142122516"
 }
